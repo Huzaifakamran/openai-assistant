dce62b0b3479305bff39781eb948d30741f2d6dc
Huzaifa Kamran <huzaifa.2014@hotmail.com>
2023-11-16 17:51:22 +0500

    adding chat history + session delete

--- app.py
+++ app.py
@@ -6,53 +6,45 @@ import os
 load_dotenv()
 
 client = OpenAI(
-    # api_key= os.getenv("OPENAI_API_KEY"),
-    api_key = st.secrets["OPENAI_API_KEY"]
+
+    # api_key=os.getenv("OPENAI_API_KEY")
+    api_key=st.secrets["OPENAI_API_KEY"]
 )
 
 def get_session_state():
     session_state = st.session_state
     if 'first_time' not in session_state:
         session_state.first_time = True
+        session_state.messages = []  # Initialize an empty list to store messages
     return session_state
 
 session_state = get_session_state()
-print(session_state)
+
 if session_state.first_time:
-    print('executed')
-    #Create an assistant. Uncomment the below code for the first time then comment it because we dont want to create assistant every-time.
-
-    # assistant = client.beta.assistants.create(
-    # instructions="You are a customer support chatbot. Use your knowledge base to best respond to customer queries.",
-    # model="gpt-4-1106-preview",
-    # tools=[{"type": "retrieval"}]
-    # )
-    
-
-    #Initializing a thread
     thread = client.beta.threads.create()
     session_state.thread_id = thread.id
     session_state.assistant_id = st.secrets["ASSISTANT_ID"]
+    # session_state.assistant_id = os.getenv("ASSISTANT_ID")
     session_state.first_time = False
 
-user_input = st.text_input(label= "Enter something:",placeholder= "Type here...")
+user_input = st.text_input(label="Enter something:", placeholder="Type here...")
 
-def create_message(query,thread_id):
+def create_message(query, thread_id):
     message = client.beta.threads.messages.create(
         thread_id=thread_id,
         role="user",
-        content= query
+        content=query
     )
 
-def bot_response(thread_id,assistant_id):
+def bot_response(thread_id, assistant_id):
     run = client.beta.threads.runs.create(
-    thread_id = thread_id,
-    assistant_id = assistant_id
+        thread_id=thread_id,
+        assistant_id=assistant_id
     )
     while True:
         run = client.beta.threads.runs.retrieve(
-        thread_id=thread_id,
-        run_id=run.id
+            thread_id=thread_id,
+            run_id=run.id
         )
         if run.status == "completed":
             messages = client.beta.threads.messages.list(thread_id=thread_id)
@@ -61,16 +53,18 @@ def bot_response(thread_id,assistant_id):
             break
     return text
 
-
 if st.button("Submit"):
-    st.write("User:",user_input)
-    with st.spinner("Processing"):
-        thread_id = session_state.thread_id 
-        assistant_id = session_state.assistant_id 
-        print(assistant_id)
-        print(thread_id)
-        message = create_message(user_input,thread_id)
-        text = bot_response(thread_id,assistant_id)
-        st.write("Bot:",text)
+    user_message = user_input
+    create_message(user_message, session_state.thread_id)
+    bot_reply = bot_response(session_state.thread_id, session_state.assistant_id)
+    session_state.messages.insert(0, ("User", user_message))
+    session_state.messages.insert(0, ("Bot", bot_reply))
 
+# Display all messages
+for role, message in session_state.messages:
+    st.write(f"{role}:", message)
 
+# Add a reset button
+if st.button("Reset Session"):
+    st.session_state.clear()
+    st.experimental_rerun()  # Rerun the app to reset the session
